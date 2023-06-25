cd1beb7f56a90582a13e8f34a091736a6ef57e09
rccodex <infcbaghi81@gmail.com>
2023-06-25 16:26:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+    // let screen = document.querySelector('.screen');
+    // let buttons = document.querySelectorAll('.btn');
+    // let clear = document.querySelector('.btn-clear');
+    // let equal = document.querySelector('.btn-equal');
+
+    // (function() {
+    //     buttons.forEach(function(button) {
+    //         button.addEventListener('click', function(e) {
+    //             let value = e.target.dataset.num;
+    //             screen.value += value;
+    //             console.log(value)
+    //         })
+    //     });
+    //     equal.addEventListener('click', function(e) {
+    //         if (screen.value === '') {
+    //             screen.value = "";
+    //         } else {
+    //             let answer = eval(screen.value);
+    //             screen.value = answer;
+    //         }
+    //     })
+
+    // })();
